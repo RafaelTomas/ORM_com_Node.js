f0578403ed45b92ac37e078c3ad59af16a0dc1a1
Rafael Tomás <rafaeltomassouza@gmail.com>
2021-12-29 20:12:26 -0300

    MVc

--- api/index.js
+++ api/index.js
@@ -1,18 +1,10 @@
 const express = require('express')
+const routes = require('./routes/index')
 
 const app = express()
-
-app.use(express.json())
-
 let port = process.env.PORT || 3000
 
-app.get('/teste', (req, res) => res
-    .status(200)
-    .send({
-        mensagem : 'Olá'
-    })
-    )
-    
+routes(app)
 
 app.listen(port, () => console.log(`servidor esta rodando na porta ${port}`))
 
